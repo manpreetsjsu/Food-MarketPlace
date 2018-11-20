dc892b5eae511c6efda8feffa602a0e9db7433ba
manpreetsjsu <manpreetsingh_er@yahoo.com>
2018-11-20 10:47:03 -0800

    edit delete buttons added to my posts - functionality not connectd with firebase (#25)
    * corrected image upload feature bug
    
    * implemented react memo feature in functional comp, changes in gridLayout
    
    * made griLayout functional, added RenderedGridElements in GridLayout
    
    * added login icon/image to loginModal
    
    * deleted package-lock.json
    
    * added filters, search by location - DIRTY CODE
    
    * New Feature added search by location, filters ready - nodejs backend for testing
    
    * loader added, filters search bug found and removed
    
    * reset location when user clicks on marketplace icon
    
    * redux implemented
    
    * firebase integration with frontend done, problem in downloading images-permission denied
    
    * login/signout, dispaly friendly msg when no posts against location/search criteria
    
    * edit delete buttons added to post- not connected backend

--- src/stateless_components/ItemClickModal/ItemClickModal.js
+++ src/stateless_components/ItemClickModal/ItemClickModal.js
@@ -1,32 +1,66 @@
-import React from 'react'
-import { Header, Image, Modal } from 'semantic-ui-react'
+import React,{Component} from 'react'
+import {Button, Header, Icon, Image, Modal} from 'semantic-ui-react'
 import {FoodItemContext} from "../../Context/LoggedInContext";
 
-const OpenModal = (props) => {
-    return (
-        <FoodItemContext.Consumer>
-            {foodItemInfo =>
-                (
-                    <>
-                        <Modal open={props.isModalOpen} trigger={props.children} centered={false}>
-                            <Modal.Header>{props.title}</Modal.Header>
-                            <Modal.Content image>
-                                <Image wrapped size='medium' src= {foodItemInfo.images} />
-                                <Modal.Description>
-                                    <Header>{foodItemInfo.title}</Header>
-                                    <p>{foodItemInfo.description}</p>
-                                    <p>{foodItemInfo.location.description}</p>
-                                    <p>${foodItemInfo.price}</p>
-                                    <p>{foodItemInfo.timestamp}</p>
-                                    <p>Buy@ {foodItemInfo.contact}</p>
-                                </Modal.Description>
-                            </Modal.Content>
-                        </Modal>
-                    </>
-            )
-            }
-        </FoodItemContext.Consumer>
-    );
+class OpenModal extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            isModalOpen:false
+        };
+    }
+
+
+    // getDerivedStateFromProps(props,state){
+    //     if(props.isModalOpen !== this.state.isModalOpen){
+    //         this.setState({isModalOpen:props.isModalOpen})
+    //     }
+    //}
+    componentDidUpdate(prevProps,prevState,snapShot){
+        if(prevProps.isModalOpen !== this.props.isModalOpen){
+            this.setState({isModalOpen:true})
+        }
+    }
+
+    closeButtonHandler=()=>{
+      this.setState({isModalOpen:false})
+    };
+    render(){
+        //console.log(this.state);
+        //console.log(this.props);
+        return (
+            <FoodItemContext.Consumer>
+                {foodItemInfo =>
+                    (
+                        <>
+                            <Modal open={this.state.isModalOpen } onClose={this.closeButtonHandler} trigger={this.props.children} centered={false} closeOnDimmerClick={true}>
+                                <Modal.Actions>
+                                    <Button color='grey' onClick={this.closeButtonHandler} >
+                                        Close
+                                    </Button>
+                                </Modal.Actions>
+                                <Modal.Header>{foodItemInfo.title}</Modal.Header>
+                                <Modal.Content image>
+                                    <Image wrapped size='medium' src= {foodItemInfo.images} />
+                                    <Modal.Description>
+                                        <Header>{foodItemInfo.title}</Header>
+                                        <p>{foodItemInfo.description}</p>
+                                        <p>{foodItemInfo.location.description}</p>
+                                        <p>${foodItemInfo.price}</p>
+                                        <p>{foodItemInfo.timestamp}</p>
+                                        <p>Buy@ {foodItemInfo.contact}</p>
+                                    </Modal.Description>
+                                </Modal.Content>
+
+                            </Modal>
+                        </>
+                    )
+                }
+            </FoodItemContext.Consumer>
+        );
+    }
+
 }
 
 export default OpenModal;
