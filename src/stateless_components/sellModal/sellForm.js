e18fbbde412bfb3f94026fa48c58e660856103e7
Sar Padda <sarpadda@Sars-MacBook-Pro.local>
2018-11-30 16:37:43 -0800

    Revert "React Async Await implemented (#30)"
    This reverts commit c348b4896f5fb8c444111fc35a49963a2edae093.

--- src/stateless_components/sellModal/sellForm.js
+++ src/stateless_components/sellModal/sellForm.js
@@ -43,7 +43,6 @@ const PostSubmissionForm=(props)=>{
 class SellForm extends Component{
     constructor(props){
         super(props);
-        this.deleteButtonHandler=this.deleteButtonHandler.bind(this);
         this.state={
             post_id: props.userInfo ? props.userInfo.post_id : '',
             title: props.userInfo ? props.userInfo.title : '',
@@ -117,7 +116,7 @@ class SellForm extends Component{
     };
 
 
-    postButtonClickHandler= ()=> {
+    postButtonClickHandler=()=> {
         // this.setState({isPostSubmitted:true},()=>{
         //     postButtonClickHandler(this.state);
         // });
@@ -126,20 +125,19 @@ class SellForm extends Component{
             if(this.validateForm()){ //validate sell form before posting
                 console.log('posting item...');
                 this.setState({clickedPostButton: true},
-                    async () => {
-                        try {
-                            const download_url = await uploadFile(this.state);
-                            const docRef = await postDataToFirebase(this.state,download_url);
-                            await Promise.all([
-                                                appendIDToPost(docRef),
-                                                querySaveCategories(this.state.category,docRef),
-                                                savePostInUserData(docRef)
-                            ]);
-                            this.setState({submissionComplete:true})
-                        } catch(e) {
-                            console.error("Problem", e);
-                            this.setState({errorInSubmission: true, clickedPostButton: false});
-                        }
+                    () => {
+                        uploadFile(this.state).then(url=>url)
+                            .then(url=>postDataToFirebase(this.state,url))
+                            .then(docRef=>{
+                                return Promise.all([
+                                    appendIDToPost(docRef),
+                                    querySaveCategories(this.state.category,docRef),
+                                    savePostInUserData(docRef)
+                                ])
+                            }).then(()=>this.setState({submissionComplete: true}))
+                            .catch((err)=>{
+                                console.log(err);
+                                this.setState({errorInSubmission:true,clickedPostButton:false})})
                     });
             }
         }
@@ -148,29 +146,26 @@ class SellForm extends Component{
                 if (this.state.oldImageUrl===""){ // user has uploaded new image while editing..
                     console.log('posting item...');
                     this.setState({clickedPostButton: true},
-                        async () => {
-                            try {
-                                const download_url = await uploadFile(this.state);
-                                await updateDataToFirebase(this.state,download_url);
-                                this.setState({submissionComplete:true})
-                            } catch(e) {
-                                console.error("Problem", e);
-                                this.setState({errorInSubmission: true, clickedPostButton: false});
-                            }
-
+                        () => {
+                            uploadFile(this.state).then(url => url)
+                                .then(url => updateDataToFirebase(this.state, url))
+                                .then(()=>this.setState({submissionComplete:true}))
+                                .catch((err) =>{
+                                    console.log(err);
+                                    this.setState({errorInSubmission: true, clickedPostButton: false}) // this will take user back to sellForm
+                                })
                         });
             }
             else{
                     //updating post with old image
                     this.setState({clickedPostButton:true},
-                        async ()=>{
-                            try {
-                                await updateDataToFirebaseOldimage(this.state);
-                                this.setState({submissionComplete:true})
-                            } catch(e) {
-                                console.error("Problem", e);
-                                this.setState({errorInSubmission: true, clickedPostButton: false});
-                            }
+                        ()=>{
+                        updateDataToFirebaseOldimage(this.state)
+                            .then(()=>this.setState({submissionComplete:true}))
+                            .catch((err)=> {
+                                console.log(err);
+                                this.setState({errorInSubmission: true, clickedPostButton: false})
+                            })
                     });
 
 
@@ -180,7 +175,7 @@ class SellForm extends Component{
 
     };
 
-    async deleteButtonHandler(){
+    deleteButtonHandler=()=>{
       // delete the item from firebase - sarang
       // then update the myPosts section - reload - Manpreet
         console.log('Deleting item...')
@@ -192,13 +187,12 @@ class SellForm extends Component{
             delete_from_category(this.state),
         ]);
 
-        try {
-           await delete_promise_calls ;
-           this.setState({submissionComplete:true})
-        } catch(e) {
-            console.error("Problem", e);
-            this.setState({errorInSubmission: true, clickedPostButton: false});
-        }
+        delete_promise_calls.then(()=>{
+            this.setState({submissionComplete:true})
+        }).catch((err)=>{
+            console.log(err);
+            this.setState({errorInSubmission: true, clickedPostButton: false})
+        })
     };
 
 
